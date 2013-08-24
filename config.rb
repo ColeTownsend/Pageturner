163680d354f70870dc0d6b762dc33134a3bcfc67
ColeTownsend <cole@coletownsend.com>
2013-08-23 22:36:32 -0400

    soemthing.

---
+++ config.rb
@@ -0,0 +1,17 @@
+require 'sass-globbing'
+
+# Require any additional compass plugins here.
+project_type = :stand_alone
+
+# Publishing paths
+http_path = "/"
+http_images_path = "/images"
+http_generated_images_path = "/images"
+css_dir = "assets/css"
+
+# Local development paths
+sass_dir = "sass"
+images_dir = "source/images"
+
+line_comments = false
+output_style = :compressed
